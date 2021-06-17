4370397aef0839099bbcd3e3d27f840c8c9485f8
fbottarel <fabrizio.bottarel@iit.it>
2021-06-17 12:18:00 +0200

    changed path to be input via command line

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,42 +1,10 @@
-#include <vtkActor.h>
-#include <vtkAxesActor.h>
-#include <vtkCaptionActor2D.h>
-#include <vtkTextActor.h>
-#include <vtkTextProperty.h>
-#include <vtkCamera.h>
-#include <vtkNamedColors.h>
-#include <vtkNew.h>
-#include <vtkPolyDataMapper.h>
-#include <vtkPolyDataReader.h>
-#include <vtkProperty.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkInteractorStyleTrackballCamera.h>
-#include <vtkRenderer.h>
-#include <vtkSmartPointer.h>
-
-#include <vtkPolyData.h>
-#include <vtkSphereSource.h>
-
-#include <urdf_model/model.h>
-#include <urdf_model/pose.h>
-#include <urdf_parser/urdf_parser.h>
-
-#include <Eigen/Core>
-#include <Eigen/Geometry>
-
-#include <fstream>
-#include <stdio.h>
-
 #include <manip-env-visu/manipulation_env.h>
-
-#include <vtkPNGReader.h>
-#include <vtkTexture.h>
-
 
 int main(int argc, char const *argv[])
 {
     std::string urdf_filename = argv[1];
+    std::string object_path = argv[2];
+    std::string object_texture = argv[3];
 
     if (!fileExists(urdf_filename))
     {
@@ -44,13 +12,9 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
-    urdf::ModelInterfaceSharedPtr gripper_urdf_model = urdf::parseURDFFile(urdf_filename);
-
-    if (gripper_urdf_model)
-        std::cout << "URDF file parsed successfully" << std::endl;
-    else
+    if (!fileExists(object_path))
     {
-        std::cout << "URDF file failed to parse" << std::endl;
+        std::cout << "There is no such mesh file" << std::endl;
         return -1;
     }
 
@@ -58,7 +22,7 @@ int main(int argc, char const *argv[])
 
     Eigen::Matrix4f object_pose = Eigen::Matrix4f::Identity();
     object_pose.col(3) << 0.2, 0.2, 0.0, 1.0;
-    manip_env.addObject("/home/fbottarel/workspace/manip-env-visu/models/objects/cracker_box/textured_simple.obj", "/home/fbottarel/workspace/manip-env-visu/models/objects/cracker_box/texture_map.png", object_pose);
+    manip_env.addObject(object_path, object_texture, object_pose);
 
     std::vector<float> joint_values = {0.04, 0.04};
     Eigen::Matrix4f root_pose = Eigen::Matrix4f::Identity();
