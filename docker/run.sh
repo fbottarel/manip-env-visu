75a05587ceb67e4938097103cd1b490ea2cd94f7
fbottarel <fabrizio.bottarel@iit.it>
2021-06-21 17:25:00 +0200

    changed cmakelists in order to make library shared and findable

--- docker/run.sh
+++ docker/run.sh
@@ -14,7 +14,7 @@ docker run \
   -e DISPLAY=$DISPLAY \
   -e QT_X11_NO_MITSHM=1 \
   fbottarel/manip-env-visu \
-  /workspace/manip-env-visu/build/examples/gripper_render /workspace/manip-env-visu/models/hands/franka_gripper/franka_gripper.urdf /workspace/manip-env-visu/models/objects/cracker_box/textured_simple.obj /workspace/manip-env-visu/models/objects/cracker_box/texture_map.png
+  /workspace/manip-env-visu/build/bin/gripper_render /workspace/manip-env-visu/models/hands/franka_gripper/franka_gripper.urdf /workspace/manip-env-visu/models/objects/cracker_box/textured_simple.obj /workspace/manip-env-visu/models/objects/cracker_box/texture_map.png
 
 # Fix xhost permissions
 xhost -local:root
