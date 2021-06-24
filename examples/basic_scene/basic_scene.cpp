237013a59620c989a3eee335734cad025d913c57
fbottarel <fabrizio.bottarel@iit.it>
2021-06-24 17:58:07 +0200

    updated example and added another

--- examples/basic_scene/basic_scene.cpp
+++ examples/basic_scene/basic_scene.cpp
@@ -1,3 +1,8 @@
+/*
+ * Show a simple scene with an object, a hand and two contact
+ * points. This example has everything this library offers.
+*/
+
 #include <manip-env-visu/manipulation_env.h>
 
 int main(int argc, char const *argv[])
@@ -20,9 +25,13 @@ int main(int argc, char const *argv[])
 
     mev::ManipulationEnv manip_env;
 
+    // Set up the object, its texture, pose and opacity
+
     Eigen::Matrix4f object_pose = Eigen::Matrix4f::Identity();
     object_pose.col(3) << 0.2, 0.2, 0.0, 1.0;
-    manip_env.addObject(object_path, object_texture, object_pose);
+    manip_env.addObject(object_path, object_texture, object_pose, 0.2);
+
+    // Spawn in a conveniently placed gripper, with proper joint values
 
     std::vector<float> joint_values = {0.04, 0.04};
     Eigen::Matrix4f root_pose = Eigen::Matrix4f::Identity();
@@ -31,6 +40,28 @@ int main(int argc, char const *argv[])
                                   1,  0,  0,
                                   0,  0,  -1;
     manip_env.addHand(urdf_filename, root_pose, joint_values);
+
+    // Show two contact points with the normalized force and friction cone
+
+    Eigen::Matrix4f contact_1 = Eigen::Matrix4f::Identity();
+    contact_1.block<3,3>(0,0) <<  0,  0,  1,
+                                  0,  1,  0,
+                                  -1,  0,  0;
+    contact_1.col(3) << 0.16, 0.2, 0.07, 1.0;
+    manip_env.addContactPoint(contact_1,
+                              0.4,
+                              true, true, true);
+
+    Eigen::Matrix4f contact_2 = Eigen::Matrix4f::Identity();
+    contact_2.block<3,3>(0,0) <<  0,  0, -1,
+                                  0,  1,  0,
+                                  1,  0,  0;
+    contact_2.col(3) << 0.24, 0.2, 0.07, 1.0;
+    manip_env.addContactPoint(contact_2,
+                              0.4,
+                              true, true, true);
+
+    // Render scene
 
     manip_env.render();
 
